c043f8ce721cf20112ee5369eb06e6aea9b217eb
Bruno Silva <scientistbruno@gmail.com>
2013-12-02 18:06:01 -0200

    Implementação de SimulatedDisk reformulada e organizada

--- src/simulatedDisk.cpp
+++ src/simulatedDisk.cpp
@@ -1,55 +1,38 @@
-#include "diskBlock.h"
+#include "simulatedDisk.h"
 
 //FIXME
 // endereco de base onde comecam os dados
 // nao sei se eh realmente eh necessario, mas por enquanto deixo assim
 int baseAddress;
 
-SimulatedDisk::SimulatedDisk(const unsigned int blocksize, blockNumber maxBlocks) : Disk(blocksize, maxBlocks)
-{
-   
-    diskBlocks[maxBlocks];
-    Disk(blocksize, maxBlocks);
-}
+SimulatedDisk::SimulatedDisk(const char *file_name,
+							 const unsigned int blocksize,
+							 DiskBlock::number maxBlocks)
+	: Storage(blocksize, maxBlocks),
+	  fileName(file_name);
 
-void Disk::Disk(const unsigned int blocksize, blockNumber maxBlocks) {
-    setBlockSize(blocksize);
-    setMaxBlocks(maxBlocks);
-}
-//FIXME
-// flush deve receber o parametro diskBlocks
-void Disk::flush(DiskBlock diskBlocks[]) {
-    
+void SimulatedDisk::flush() {
+
     for(int blockIdx=0; blockIdx < maxBlocks; blockIdx++) {
         diskBlocks[blockIdx].writeBytes((unsigned short) (baseAddress + (blockNumber * blocksize)), (unsigned short) blocksize, 0);
     }
 }
 
-//FIXME
-// nao vejo necessidade de ultimo parametro, sendo que cada diskBlock possui 
-// um private member : rawData
-// por enquanto passo 0
-void Disk::readBlock(const blockNumber bn, DiskBlock* block) {
-    block->readBytes((unsigned short) (baseAddress + (blockNumber * blocksize)), (unsigned short) blocksize, 0);
+// FIXME
+@SuppressWarning("all") // No return statement
+DiskBlock* SimulatedDisk::readBlock(const DiskBlock::number n) {
+	// TODO: Procurar pelo bloco de número n e retorná-lo!
 }
 
-//FIXME
-void Disk::writeBlock(const blockNumber bn, DiskBlock* block) {
-	block->readBytes((unsigned short) (baseAddress + (blockNumber * blocksize)), (unsigned short) blocksize, 0);
+// FIXME
+void SimulatedDisk::writeBlock(const DiskBlock::number n, DiskBlock* block_buffer) {
+	// TODO: Escrever o conteúdo de block_buffer no bloco de número n
 }
 
-void Disk::setBlockSize(const unsigned int blocksize) {
-    this->blocksize = blocksize;
+unsigned int Storage::getBlockSize() {
+    return blockSize;
 }
 
-void Disk::setMaxBlocks(const blockNumber maxBlocks) {
-    this->maxBlocks = maxBlocks;
-}
-
-unsigned int Disk::getBlockSize() {
-    return blocksize;
-}
-
-unsigned int Disk::getMaxBlocks() {
+unsigned int Storage::getMaxBlocks() {
     return maxBlocks;
 }
