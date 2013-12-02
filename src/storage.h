9c810603ba2e8850f435fe5afcc24afd0ad4a000
Bruno Silva <scientistbruno@gmail.com>
2013-12-02 17:54:46 -0200

    mv disk.h storage.h, além de simplificação de código

---
+++ src/storage.h
@@ -0,0 +1,51 @@
+#ifndef STORAGE_H
+#define	STORAGE_H
+
+#include "diskBlock.h"
+
+/**
+ * Abstract class that represents a disk and low level software I/O operations
+ * @param blockSize, size in bytes of one disk block.
+ * @param maxBlocks, maximum number of blocks in disk
+ */
+class Storage {
+public:
+
+    Storage(const unsigned int blockSize, const unsigned int maxBlocks);
+
+    /**
+     * Save structures in ram into disk
+     */
+    virtual void flush() = 0;
+
+    /**
+     * @param n, The number of the block to be written
+     * @param block_buffer, A pointer to the disk block to be written.
+     */
+    virtual void writeBlock(const DiskBlock::number n, DiskBlock* block_buffer) = 0;
+    
+    /**
+     * @param n, The number of the block to be read
+     * @return A pointer to the disk block read
+     */
+    virtual DiskBlock* readBlock(const DiskBlock::number n) = 0;
+    
+    /**
+     * Returns the size of one disk block
+     */
+    virtual unsigned int getBlockSize() = 0;
+    
+    /**
+     * Returns the maximum number of blocks in disk
+     */  
+    virtual unsigned int getMaxBlocks() = 0;
+    
+    virtual ~Storage() = 0;
+    
+protected:
+    /* Attributes available to derived classes */
+    unsigned int blockSize;
+    unsigned int maxBlocks;
+};
+
+#endif	/* STORAGE_H */
