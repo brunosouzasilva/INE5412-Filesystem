28d180c6278c84f6007861e340b0ec1d681566e1
Bruno Silva <scientistbruno@gmail.com>
2013-12-04 00:23:46 -0200

    Consertando construtores, objetos da primeira camada
    Contribuições de Pedro Lenzi

--- src/simulatedDisk.cpp
+++ src/simulatedDisk.cpp
@@ -7,16 +7,18 @@ int baseAddress;
 
 SimulatedDisk::SimulatedDisk(const char *file_name,
 							 const unsigned int blocksize,
-							 DiskBlock::number maxBlocks)
-	: Storage(blocksize, maxBlocks),
-	  fileName(file_name);
+							 DiskBlock::number maxBlocks): Storage(blocksize, maxBlocks) {
+	fileName = file_name;
+}
 
+/* FIXME??
 void SimulatedDisk::flush() {
 
     for(int blockIdx=0; blockIdx < maxBlocks; blockIdx++) {
         diskBlocks[blockIdx].writeBytes((unsigned short) (baseAddress + (blockNumber * blocksize)), (unsigned short) blocksize, 0);
     }
 }
+*/
 
 // FIXME
 @SuppressWarning("all") // No return statement
--- src/storage.h
+++ src/storage.h
@@ -11,41 +11,48 @@
 class Storage {
 public:
 
-    Storage(const unsigned int blockSize, const unsigned int maxBlocks);
+	explicit
+	Storage(const unsigned int blockSize, const unsigned int maxBlocks) {
+		this->blockSize = blockSize;
+		this->maxBlocks = maxBlocks;
+	}
 
-    /**
-     * Save structures in ram into disk
-     */
-    virtual void flush() = 0;
+	virtual ~Storage() = 0;
 
-    /**
-     * @param n, The number of the block to be written
-     * @param block_buffer, A pointer to the disk block to be written.
-     */
-    virtual void writeBlock(const DiskBlock::number n, DiskBlock* block_buffer) = 0;
-    
-    /**
-     * @param n, The number of the block to be read
-     * @return A pointer to the disk block read
-     */
-    virtual DiskBlock* readBlock(const DiskBlock::number n) = 0;
-    
-    /**
-     * Returns the size of one disk block
-     */
-    virtual unsigned int getBlockSize() = 0;
-    
-    /**
-     * Returns the maximum number of blocks in disk
-     */  
-    virtual unsigned int getMaxBlocks() = 0;
-    
-    virtual ~Storage() = 0;
-    
+	/**
+	 * Save structures in ram into disk
+	 */
+	virtual void flush() = 0;
+
+	/**
+	 * @param n, The number of the block to be written
+	 * @param block_buffer, A pointer to the disk block to be written.
+	 */
+	virtual void writeBlock(const DiskBlock::number n,
+			DiskBlock* block_buffer) = 0;
+
+	/**
+	 * @param n, The number of the block to be read
+	 * @return A pointer to the disk block read
+	 */
+	virtual DiskBlock* readBlock(const DiskBlock::number n) = 0;
+
+	/**
+	 * Returns the size of one disk block
+	 */
+	virtual unsigned int getBlockSize() = 0;
+
+	/**
+	 * Returns the maximum number of blocks in disk
+	 */
+	virtual unsigned int getMaxBlocks() = 0;
+
+	virtual ~Storage() = 0;
+
 protected:
-    /* Attributes available to derived classes */
-    unsigned int blockSize;
-    unsigned int maxBlocks;
+	/* Attributes available to derived classes */
+	unsigned int blockSize;
+	unsigned int maxBlocks;
 };
 
 #endif	/* STORAGE_H */
